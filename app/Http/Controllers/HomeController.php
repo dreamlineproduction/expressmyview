cc5238b773c7ac24e5f251c9df30011b1ff72d4c
dreamlineproduction <info@dreamlineproduction.in>
2021-03-21 18:39:22 +0530

    For mobile app

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -54,4 +54,65 @@ class HomeController extends Controller
         );
         return view('home', $viewData);
     }
+
+    public function getChannelName($channelId){
+        $channel = Channel::find($channelId);
+
+        if($channel){
+            return $channel->name;
+        }else{
+            return "";
+        }
+    }
+
+    public function getHomeDatas(Request $request)
+    {
+        $id = $request->id;
+        //        $categories = Category::where('status', 1)->withCount('videos')->get();
+        $popularChannels = Channel::where('status', 1)->whereNotIn('user_id', [$id])->orderBy('subscribers', 'DESC')->take(10)->get();
+        $latestVideoPodcasts = Podcast::where('privacy', 1)->where('file_type', "video")->whereNotIn('user_id', [$id])->orderBy('created_at', 'DESC')->take(4)->get();
+        // $liveStreams =  LiveStream::where('status', 1)->whereNotIn('user_id', [$id])->orderBy('created_at', 'DESC')->take(4)->get();
+
+        foreach ($popularChannels as $popularChannel ){
+            if($popularChannel['logo']){
+                $popularChannel["path"] = Storage::disk('s3')->url("public/users/logo/".$popularChannel['logo']);
+            }
+        }
+
+        foreach ($latestVideoPodcasts as $latestVideoPodcast ){
+            if($latestVideoPodcast['thumbnail']){
+                $latestVideoPodcast["path"] = Storage::disk('s3')->url("public/podcast/thumbnail/".$latestVideoPodcast['thumbnail']);
+            }
+            $latestVideoPodcast["channelName"] = $this->getChannelName($latestVideoPodcast["channel_id"]);
+            $latestVideoPodcast["dateDiff"] = $latestVideoPodcast['created_at']->diffForHumans();
+        }
+
+        // foreach ($liveStreams as $liveStream ){
+        //     if($liveStream['thumbnail']){
+        //         $liveStream["path"] = Storage::disk('s3')->url("public/podcast/thumbnail/".$liveStream['thumbnail']);
+        //     }
+        //     $liveStream["channelName"] = $this->getChannelName($liveStream["channel_id"]);
+        //     $liveStream["dateDiff"] = $liveStream['created_at']->diffForHumans();
+        // }
+
+        if(empty($popularChannels) && empty($popularChannels) && empty($popularChannels)){
+            return response()->json([
+                'error' => "No data available right now, please try again later"
+            ]);
+        }else{
+            $viewData = array(
+                'popularChannels' => $popularChannels,
+                'latestVideoPodcasts' => $latestVideoPodcasts,
+                // 'liveStreams' => $liveStreams,
+            );
+        }
+
+        return response()->json([
+            'status' => 200,
+            $viewData
+        ]);
+    }
 }
+
+
+
