41482e7b85993962bc711179affc037528ae9747
dreamlineproduction <info@dreamlineproduction.in>
2021-03-24 21:01:09 +0530

    Make some chnages in Broadcast index page

--- public/js/search.js
+++ public/js/search.js
@@ -108,7 +108,7 @@ $(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/rudra/freelance/emv/expressmyview/resources/js/search.js */"./resources/js/search.js");
+module.exports = __webpack_require__(/*! D:\xampp\htdocs\expressmyview-git\resources\js\search.js */"./resources/js/search.js");
 
 
 /***/ })
