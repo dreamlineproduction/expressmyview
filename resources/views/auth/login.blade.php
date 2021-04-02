ba8089d63752a5039162b88802e2e71dd870a042
dreamlineproduction <info@dreamlineproduction.in>
2021-04-02 18:30:54 +0530

    Changed login screen, added cookie notice

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,117 +1,129 @@
 <!DOCTYPE html>
 <html lang="en">
-   
+
 <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <meta name="description" content="Askbootstrap">
-      <meta name="author" content="Askbootstrap">
-      <title>{{ env('APP_NAME') }} - Sign in</title>
-      <!-- Favicon Icon -->
-      <link rel="icon" type="image/png" href="img/favicon.png">
-      <!-- Bootstrap core CSS-->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Askbootstrap">
+    <meta name="author" content="Askbootstrap">
+    <title>{{ env('APP_NAME') }} - Sign in</title>
+    <!-- Favicon Icon -->
+    <link rel="icon" type="image/png" href="img/favicon.png">
+    <!-- Bootstrap core CSS-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <!-- Custom fonts for this template-->
     <script src="https://kit.fontawesome.com/59c9493eb5.js" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+
     <!-- Custom styles for this template-->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <!-- Owl Carousel -->
     <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
-   </head>
-   <body class="login-main-body">
-      <section class="login-main-wrapper">
-         <div class="container-fluid pl-0 pr-0">
-            <div class="row no-gutters">
-               <div class="col-md-5 p-5 bg-white full-height">
-                  <div class="login-main-left">
-                     <div class="text-center mb-5 login-main-left-header pt-4">
-                        <img src="{{ asset('img/favicon.png') }}" class="img-fluid" alt="LOGO">
-                        <h5 class="mt-3 mb-3">Welcome to {{ env('APP_NAME') }}</h5>
-                        <p>It is a long established fact that a reader <br> will be distracted by the readable.</p>
-                     </div>
-                    <form action="{{ route('login') }}" method="post" id="login_form">
-                        @csrf
-                        <div class="form-group">
-                           <label for="email">{{ __('Email Address') }}</label>
-                           <input type="email" name="email" id="email" class="form-control" placeholder="Enter email address">
-                        </div>
-                        <div class="form-group">
-                           <label for="password">{{ __('Password') }}</label>
-                           <input type="password" name="password" id="password" class="form-control" placeholder="Password">
-                        </div>
-                        <div class="form-group">
-							<input type="checkbox" name="remember" id="remember">
-                           <label for="remember">{{ __('Remember Me') }}</label>
-                           
-                        </div>
-                        <div class="mt-4">
-                           <div class="row">
-                              <div class="col-12">
-                                 <button type="submit" class="btn btn-outline-primary btn-block btn-lg">{{ __('Sign In') }}</button>
-                              </div>
-                           </div>
-                        </div>
-                     </form>
-                     <div class="text-center mt-5">
-                        <p class="light-gray">Don’t have an account? <a href="{{ route('register') }}">{{ __('Sign Up') }}</a></p>
-                     </div>
-					  
-					  <div class="text-center mt-2">
-                        <p class="light-gray">{{ __('Forgot Your Password?') }}? <a href="{{ route('password.request') }}">{{ __('Click Here') }}</a></p>
-                     </div>
+</head>
 
-                     
-					  
-					  <div class="text-center mt-5">
-                        <p class="light-gray"> <a href="{{ route('home') }}"><i class="fas fa-long-arrow-alt-left"></i> {{ __('Back to Home') }}</a></p>
-                     </div>
-                  </div>
-               </div>
-               <div class="col-md-7">
-                  <div class="login-main-right bg-white p-5 mt-5 mb-5">
-                     <div class="owl-carousel owl-carousel-login">
-                        <div class="item">
-                           <div class="carousel-login-card text-center">
-                              <img src="img/login_image1.svg" class="img-fluid" alt="LOGO">
-                              <h5 class="mt-5 mb-3">Watch Live Streams</h5>
-                              <p class="mb-4">when an unknown printer took a galley of type and scrambled<br> it to make a type specimen book. It has survived not <br>only five centuries</p>
-                           </div>
+<body class="login-main-body">
+    <section class="login-main-wrapper">
+        <div class="container-fluid pl-0 pr-0">
+            <div class="row no-gutters">
+                <div class="col-md-5 p-5 bg-white full-height">
+                    <div class="login-main-left">
+                        <div class="text-center mb-5 login-main-left-header pt-4">
+                            <img src="{{ asset('img/favicon.png') }}" class="img-fluid" alt="LOGO">
+                            <h5 class="mt-3 mb-3">Welcome to {{ env('APP_NAME') }}</h5>
+                            <p>It is a long established fact that a reader <br> will be distracted by the readable.</p>
                         </div>
-                        <div class="item">
-                           <div class="carousel-login-card text-center">
-                              <img src="img/login_image2.svg" class="img-fluid" alt="LOGO">
-                              <h5 class="mt-5 mb-3">View Millions of Podcasts</h5>
-                              <p class="mb-4">when an unknown printer took a galley of type and scrambled<br> it to make a type specimen book. It has survived not <br>only five centuries</p>
-                           </div>
+                        <form action="{{ route('login') }}" method="post" id="login_form">
+                            @csrf
+                            <div class="form-group">
+                                <label for="email">{{ __('Email Address') }}</label>
+                                <input type="email" name="email" id="email" class="form-control"
+                                    placeholder="Enter email address">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">{{ __('Password') }}</label>
+                                <input type="password" name="password" id="password" class="form-control"
+                                    placeholder="Password">
+                            </div>
+                            <div class="form-group">
+                                <input type="checkbox" name="remember" id="remember">
+                                <label for="remember">{{ __('Remember Me') }}</label>
+
+                            </div>
+                            <div class="mt-4">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <button type="submit"
+                                            class="btn btn-outline-primary btn-block btn-lg">{{ __('Sign In') }}</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                        <div class="text-center mt-5">
+                            <p class="light-gray">Don’t have an account? <a
+                                    href="{{ route('register') }}">{{ __('Sign Up') }}</a></p>
                         </div>
-                        <div class="item">
-                           <div class="carousel-login-card text-center">
-                              <img src="img/login_image3.svg" class="img-fluid" alt="LOGO">
-                              <h5 class="mt-5 mb-3">Get Notified from your Subscription</h5>
-                              <p class="mb-4">when an unknown printer took a galley of type and scrambled<br> it to make a type specimen book. It has survived not <br>only five centuries</p>
-                           </div>
+
+                        <div class="text-center mt-2">
+                            <p class="light-gray">{{ __('Forgot Your Password?') }}? <a
+                                    href="{{ route('password.request') }}">{{ __('Click Here') }}</a></p>
                         </div>
-                     </div>
-                  </div>
-               </div>
+
+
+
+                        <div class="text-center mt-5">
+                            <p class="light-gray"> <a href="{{ route('home') }}"><i
+                                        class="fas fa-long-arrow-alt-left"></i> {{ __('Back to Home') }}</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="login-main-right bg-white p-5 mt-5 mb-5">
+                        <div class="owl-carousel owl-carousel-login">
+                            <div class="item">
+                                <div class="carousel-login-card text-center">
+                                    <img src="img/login_image1.svg" class="img-fluid" alt="LOGO">
+                                    <h5 class="mt-5 mb-3">Watch Live Streams</h5>
+                                    <p class="mb-4">when an unknown printer took a galley of type and scrambled<br> it
+                                        to make a type specimen book. It has survived not <br>only five centuries</p>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="carousel-login-card text-center">
+                                    <img src="img/login_image2.svg" class="img-fluid" alt="LOGO">
+                                    <h5 class="mt-5 mb-3">View Millions of Podcasts</h5>
+                                    <p class="mb-4">when an unknown printer took a galley of type and scrambled<br> it
+                                        to make a type specimen book. It has survived not <br>only five centuries</p>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="carousel-login-card text-center">
+                                    <img src="img/login_image3.svg" class="img-fluid" alt="LOGO">
+                                    <h5 class="mt-5 mb-3">Get Notified from your Subscription</h5>
+                                    <p class="mb-4">when an unknown printer took a galley of type and scrambled<br> it
+                                        to make a type specimen book. It has survived not <br>only five centuries</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-         </div>
-      </section>
-      <!-- Bootstrap core JavaScript-->
-<script src="{{ asset('js/app.js') }}" defer></script>
-<!-- Core plugin JavaScript-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"
+        </div>
+    </section>
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Core plugin JavaScript-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"
         integrity="sha256-H3cjtrm/ztDeuhCN9I4yh4iN2Ybx/y1RM7rMmAesA0k=" crossorigin="anonymous" defer></script>
-<!-- Owl Carousel -->
-<script src="{{ asset('js/owl.carousel.min.js') }}" defer></script>
-<!-- jQuery Validation -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js" defer></script>
-<!-- Custom scripts for all pages-->
-<script src="{{ asset('js/custom.js') }}" defer></script>
-<script src="{{ asset('js/login.js') }}" defer></script>
-   </body>
+    <!-- Owl Carousel -->
+    <script src="{{ asset('js/owl.carousel.min.js') }}" defer></script>
+    <!-- jQuery Validation -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js" defer></script>
+    <!-- Custom scripts for all pages-->
+    <script src="{{ asset('js/custom.js') }}" defer></script>
+    <script src="{{ asset('js/login.js') }}" defer></script>
+</body>
 
 </html>
