565711f60c661f3d3815ab3db416c9f2f9d92f61
Ayan Mukhopadhyay-iMac <info@dreamlineproduction.in>
2021-04-04 19:27:43 +0530

    Make production build

--- config/laravel-ffmpeg.php
+++ config/laravel-ffmpeg.php
@@ -2,18 +2,22 @@
 
 return [
     'ffmpeg' => [
-        // Local Path
+        // Local Path Windows
         // 'binaries' => env('FFMPEG_BINARIES', 'C:\ffmpeg\bin\ffmpeg.exe'),
+        // Local Path Mac
+        'binaries' => env('FFMPEG_BINARIES', '/usr/local/bin/ffmpeg'),
         // Server Path
-        'binaries' => env('FFMPEG_BINARIES', '/usr/bin/ffmpeg'),
+        // 'binaries' => env('FFMPEG_BINARIES', '/usr/bin/ffmpeg'),
         'threads'  => 12,
     ],
 
     'ffprobe' => [
-        // Local Path
+        // Local Path Windows
         // 'binaries' => env('FFMPEG_BINARIES', 'C:\ffmpeg\bin\ffmpeg.exe'),
+        // Local Path Mac
+        'binaries' => env('FFMPEG_BINARIES', '/usr/local/bin/ffmpeg'),
         // Server Path
-        'binaries' => env('FFPROBE_BINARIES', '/usr/bin/ffprobe'),
+        // 'binaries' => env('FFPROBE_BINARIES', '/usr/bin/ffprobe'),
     ],
 
     'timeout' => 3600,
